1094984be504559ce5482a76b5a1e5907c487ee6
gomgomgit <thechocohead@gmail.com>
2022-05-25 21:41:21 +0700

    ujian onlien

--- src/layout/aside/tabs/Lms.vue
+++ src/layout/aside/tabs/Lms.vue
@@ -11,33 +11,39 @@
   <div class="card-body pt-5">
     <template v-for="(item, index) in items" :key="index">
       <!--begin::Item-->
-      <div
-        :class="{ 'mb-7': items.length - 1 !== index }"
-        class="d-flex align-items-center"
+      <router-link
+        class="menu-link"
+        active-class="active"
+        :to="item.route"
       >
-        <!--begin::Symbol-->
-        <div class="symbol symbol-50px me-5">
-          <span :class="`bg-light-${item.color}`" class="symbol-label">
-            <span
-              :class="`svg-icon-${item.color}`"
-              class="svg-icon svg-icon-2x"
-            >
-              <inline-svg :src="item.icon" />
+        <div
+          :class="{ 'mb-7': items.length - 1 !== index }"
+          class="d-flex align-items-center"
+        >
+          <!--begin::Symbol-->
+          <div class="symbol symbol-50px me-5">
+            <span :class="`bg-light-${item.color}`" class="symbol-label">
+              <span
+                :class="`svg-icon-${item.color}`"
+                class="svg-icon svg-icon-2x"
+              >
+                <inline-svg :src="item.icon" />
+              </span>
             </span>
-          </span>
-        </div>
-        <!--end::Symbol-->
+          </div>
+          <!--end::Symbol-->
 
-        <!--begin::Text-->
-        <div class="d-flex flex-column">
-          <a href="#" class="text-dark text-hover-primary fs-6 fw-bolder">{{
-            item.title
-          }}</a>
+          <!--begin::Text-->
+          <div class="d-flex flex-column">
+            <a href="#" class="text-dark text-hover-primary fs-6 fw-bolder">{{
+              item.title
+            }}</a>
 
-          <span class="text-muted fw-bold">{{ item.description }}</span>
+            <span class="text-muted fw-bold">{{ item.description }}</span>
+          </div>
+          <!--end::Text-->
         </div>
-        <!--end::Text-->
-      </div>
+      </router-link>
       <!--end::Item-->
     </template>
   </div>
@@ -57,30 +63,35 @@ export default defineComponent({
         color: "success",
         title: "Tugas Offline",
         description: "Pengerjaan tugas secara offline",
+        route: "/lms"
       },
       {
         icon: "media/icons/duotune/electronics/elc001.svg",
         color: "warning",
         title: "Ujian Online",
         description: "Pengerjaan ujian secara online",
+        route: "/ujian-online"
       },
       {
         icon: "media/icons/duotune/communication/com012.svg",
         color: "primary",
         title: "Bank Soal",
         description: "Kumpulan soal-soal pembelajaran",
+        route: "/lms"
       },
       {
         icon: "media/icons/duotune/coding/cod008.svg",
         color: "danger",
         title: "Materi Belajar",
         description: "Kumpulan materi pembelajaran",
+        route: "/lms"
       },
       {
         icon: "media/icons/duotune/general/gen049.svg",
         color: "info",
         title: "Laporan Nilai",
         description: "Rekap nilai tugas dan ujian",
+        route: "/lms"
       },
     ];
 
