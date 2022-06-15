2a7e4d57f8e0cd8f7b0b981f539c253c6a31e0da
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-06-15 15:58:31 +0800

    add jadwal pelajaran crud

--- src/views/sekolah/mata-pelajaran/jadwal-pelajaran/Index.vue
+++ src/views/sekolah/mata-pelajaran/jadwal-pelajaran/Index.vue
@@ -2,21 +2,15 @@
 import { reactive, onMounted, watch, ref } from 'vue'
 import { request } from '@/util'
 import { setCurrentPageBreadcrumbs } from '@/core/helpers/breadcrumb';
+import { dayList } from '@/constant'
 
 import ServerSideTable from '@/components/ServerSideTable.vue'
 import FilterSelect from '@/components/filter-select/index.vue'
-
-const daysMap = {
-	1: 'Senin',
-	2: 'Selasa',
-	3: 'Rabu',
-	4: 'Kamis',
-	5: 'Jum\'at',
-	6: 'Sabtu',
-	7: 'Minggu',
-}
+import FormModal from './FormModal.vue'
 
 const tableRef = ref()
+const formMode = ref('')
+const activeData = ref({})
 
 const filterData = reactive({
 	options: {
@@ -46,7 +40,7 @@ const tableData = reactive({
 		{ label: 'KELAS', field: 'kelas_nama' },
 		{ label: 'NAMA MAPEL', field: 'mapel_nama' },
 		{ label: 'GURU', field: 'user_nama' },
-		{ label: 'HARI', field: 'jadwal_hari', formatFn: val => daysMap[val] },
+		{ label: 'HARI', field: 'jadwal_hari', formatFn: val => dayList[val] },
 		{ label: 'ACTION', field: 'action', sortable: false, width: '200px' },
 	],
 	rows: [],
@@ -65,6 +59,16 @@ function getTableData(payload) {
 	})
 }
 
+function handleEdit(row) {
+	activeData.value = row
+	formMode.value = 'Edit Data'
+}
+
+function handleFormClose(row) {
+	activeData.value = {}
+	formMode.value = ''
+}
+
 watch(
 	filterData.selected,
 	() => tableRef.value.loadItems(),
@@ -78,75 +82,83 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="card mb-5 mb-xxl-8">
-		<div class="card-body">
-
-			<section class="d-flex flex-wrap justify-content-between align-items-center mb-5">
-				<section class="d-flex flex-wrap gap-4">
-					<FilterSelect v-model:filterValue="filterData.selected.kelas" placeholder="Pilih Kelas" class="w-150px">
-						<el-option
-							v-for="kelas in filterData.options.kelas"
-							:key="kelas.kelas_id"
-							:value="kelas.kelas_id"
-							:label="kelas.kelas_nama" />
-					</FilterSelect>
-					<FilterSelect v-model:filterValue="filterData.selected.mapel" placeholder="Pilih Mapel" class="w-150px">
-						<el-option
-							v-for="mapel in filterData.options.mapel"
-							:key="mapel.mapel_id"
-							:value="mapel.mapel_id"
-							:label="mapel.mapel_nama" />
-					</FilterSelect>
-					<FilterSelect v-model:filterValue="filterData.selected.user" placeholder="Pilih Guru" class="w-150px">
-						<el-option
-							v-for="user in filterData.options.user"
-							:key="user.user_id"
-							:value="user.user_id"
-							:label="user.user_nama" />
-					</FilterSelect>
-					<FilterSelect v-model:filterValue="filterData.selected.hari" placeholder="Pilih Hari" class="w-150px">
-						<el-option
-							v-for="(day, dayId) in daysMap"
-							:key="dayId"
-							:value="dayId"
-							:label="day" />
-					</FilterSelect>
-				</section>
-
-				<section class="d-flex flex-wrap gap-4">
-					<button class="btn btn-primary d-flex gap-1 align-items-center">
-						<i class="bi bi-plus fs-1"></i>
-						Import
-					</button>
-					<button class="btn btn-primary d-flex gap-1 align-items-center">
-						<i class="bi bi-plus fs-1"></i>
-						Tambah Jadwal
-					</button>
-				</section>
-			</section>
-
-			<ServerSideTable
-				ref="tableRef"
-				:totalRows="tableData.totalRows || 0"
-				:columns="tableData.columns"
-				:rows="tableData.rows"
-				@loadItems="getTableData">
-				<template #table-row="{column, row}">
-					<div v-if="column.field == 'action'">
-						<button class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2">
-							<span class="svg-icon svg-icon-3">
-								<inline-svg src="media/icons/duotune/art/art005.svg" />
-							</span>
+	<div>
+		<div class="card mb-5 mb-xxl-8">
+			<div class="card-body">
+				<section class="d-flex flex-wrap justify-content-between align-items-center mb-5">
+					<section class="d-flex flex-wrap gap-4">
+						<FilterSelect v-model:filterValue="filterData.selected.kelas" placeholder="Pilih Kelas" class="w-150px">
+							<el-option
+								v-for="kelas in filterData.options.kelas"
+								:key="kelas.kelas_id"
+								:value="kelas.kelas_id"
+								:label="kelas.kelas_nama" />
+						</FilterSelect>
+						<FilterSelect v-model:filterValue="filterData.selected.mapel" placeholder="Pilih Mapel" class="w-150px">
+							<el-option
+								v-for="mapel in filterData.options.mapel"
+								:key="mapel.mapel_id"
+								:value="mapel.mapel_id"
+								:label="mapel.mapel_nama" />
+						</FilterSelect>
+						<FilterSelect v-model:filterValue="filterData.selected.user" placeholder="Pilih Guru" class="w-150px">
+							<el-option
+								v-for="user in filterData.options.user"
+								:key="user.user_id"
+								:value="user.user_id"
+								:label="user.user_nama" />
+						</FilterSelect>
+						<FilterSelect v-model:filterValue="filterData.selected.hari" placeholder="Pilih Hari" class="w-150px">
+							<el-option
+								v-for="(day, dayId) in dayList"
+								:key="dayId"
+								:value="dayId"
+								:label="day" />
+						</FilterSelect>
+					</section>
+					<section class="d-flex flex-wrap gap-4">
+						<button class="btn btn-primary d-flex gap-1 align-items-center">
+							<i class="bi bi-plus fs-1"></i>
+							Import
 						</button>
-						<button class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
-							<span class="svg-icon svg-icon-3">
-								<inline-svg src="media/icons/duotune/general/gen027.svg" />
-							</span>
+						<button
+							class="btn btn-primary d-flex gap-1 align-items-center"
+							@click="formMode = 'Tambah Data'">
+							<i class="bi bi-plus fs-1"></i>
+							Tambah Jadwal
 						</button>
-					</div>
-				</template>
-			</ServerSideTable>
-
+					</section>
+				</section>
+				<ServerSideTable
+					ref="tableRef"
+					:totalRows="tableData.totalRows || 0"
+					:columns="tableData.columns"
+					:rows="tableData.rows"
+					@loadItems="getTableData">
+					<template #table-row="{column, row}">
+						<div v-if="column.field == 'action'">
+							<button
+								class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2"
+								@click="handleEdit(row)">
+								<span class="svg-icon svg-icon-3">
+									<inline-svg src="media/icons/duotune/art/art005.svg" />
+								</span>
+							</button>
+							<button class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
+								<span class="svg-icon svg-icon-3">
+									<inline-svg src="media/icons/duotune/general/gen027.svg" />
+								</span>
+							</button>
+						</div>
+					</template>
+				</ServerSideTable>
+			</div>
 		</div>
+
+		<FormModal
+			:mode="formMode"
+			:activeData="activeData"
+			@close="handleFormClose"
+			@submit="tableRef.loadItems()" />
 	</div>
 </template>
