44d16cf22d96fe2bf8222b950b7eb32ce951dba0
AbidKhairyAK <abid.khairy.ak@gmail.com>
2022-06-18 09:01:38 +0800

    fix: ckeditor v-model

--- src/components/ckeditor/Index.vue
+++ src/components/ckeditor/Index.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { onMounted, reactive, ref } from 'vue';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { computed } from '@vue/reactivity';
 
@@ -9,24 +8,18 @@ const props = defineProps({
 
 const emits = defineEmits(['update:editorValue'])
 
-
 const editor = ClassicEditor
-const editorData = ref()
-// const editorData = computed(()=>{
-//   if (props.editorValue) {
-//     return editorData.value = props.editorValue  
-//   }
-// })
+
+const editorData = computed({
+  get: () => props.editorValue ?? '',
+  set: (val) => emits('update:editorValue', val)
+})
+
 const editorConfig = {
     // The configuration of the editor.
-}
-function updateValue() {
-  emits('update:editorValue', editorData.value)
 }
 </script>
 
 <template>
-<div>
-  <ckeditor class="w-100" :editor="editor" v-model="editorData" :config="editorConfig" v-on:input="updateValue()"></ckeditor>
-</div>
+  <ckeditor class="w-100" :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
 </template>
