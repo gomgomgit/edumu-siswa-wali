5ed0675df1d1b5331de3cffaab6fb8bf0ba149dc
gomgomgit <thechocohead@gmail.com>
2022-07-01 08:37:55 +0700

    video data to modal

---
+++ src/views/lms/materi-belajar/video/FormModal.vue
@@ -0,0 +1,156 @@
+<script setup>
+import { reactive, watch } from "vue"
+import qs from 'qs'
+import { useToast } from "vue-toast-notification"
+import { isEmpty } from "validate.js"
+
+import Modal from "@/components/modals/CustomModal.vue"
+import { request } from "@/util";
+
+const props = defineProps({
+	mode: { type: String, required: true },
+	activeData: { type: Object, required: true },
+	dataOption: Object
+})
+
+const emits = defineEmits(['close', 'submit'])
+
+const initialForm = { 
+  materi_id: '',
+  kelas_id: '',
+  mapel_id: '',
+  user_id: '',
+  materi_judul: '',
+  materi_status: '',
+  materi_file: null,
+}
+
+const form = reactive({...initialForm})
+
+function handleClose () {
+	Object.assign(form, {...initialForm})
+	emits('close')
+}
+
+function handleSubmit () {
+  let selectedClass = ''
+  if (form.kelas_id.includes('all')) {
+    selectedClass = kelasOption.value.map(function (obj) {
+      return obj.kelas_id
+    })
+  } else {
+    selectedClass = form.kelas_id
+  }
+  
+  const formData = new FormData()
+  formData.append('materi_id', form.materi_id)
+  formData.append('kelas_id', selectedClass)
+  formData.append('mapel_id', form.mapel_id)
+  formData.append('materi_judul', form.materi_judul)
+  formData.append('materi_status', form.materi_status)
+  formData.append('materi_file', form.materi_file)
+  formData.append('materi_tipe', 'video')
+
+  const endpoint = props.activeData ? 'video/edit' : 'video/add'
+  const message = props.activeData ? 'Data Berhasil Diedit!' : 'Data Berhasil Ditambahkan!'
+  request.post(endpoint, formData, {
+    headers: {
+      'Content-Type' : 'multipart/form-data'
+    }
+  }).then(res => {
+      Object.assign(form, initialForm)
+      useToast().success(message)
+      emits('submit')
+      emits('close')
+  })
+}
+
+watch(
+	() => props.activeData,
+	activeData => !isEmpty(activeData) && Object.assign(form, { ...activeData, materi_file: 'https://www.youtube.com/watch?v=' + activeData.materi_file, kelas_id: activeData.kelas_id.split(",").map( Number ) }),
+	{ deep: true }
+)
+</script>
+
+<template>
+	<Modal
+		:title="props.mode"
+		:show="props.mode"
+		:breadcrumb="Array('Sekolah', 'Mata Pelajaran', 'Data Mapel', props.mode)"
+		@closeModal="handleClose"
+		@confirm="handleSubmit"
+		@dismiss="handleClose">
+    <div class="d-flex flex-column gap-4">
+      <div class="row">
+        <div class="col-3 d-flex">
+          <p class="m-0 fs-4 fw-bold">Kelas</p>
+        </div>
+        <div class="col-9 align-items-center d-flex">
+          <el-select
+            v-model="form.kelas_id"
+            multiple
+            placeholder="Pilih Kelas"
+            style="width: 100%"
+            filterable
+          >
+            <el-option label="Pilih Semua" value="all"
+            />
+            <el-option
+              v-for="kelas in props.dataOption.kelasOption"
+              :key="kelas.kelas_id"
+              :label="kelas.kelas_nama"
+              :value="kelas.kelas_id"
+            />
+          </el-select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 d-flex">
+          <p class="m-0 fs-4 fw-bold">Mata Pelajaran</p>
+        </div>
+        <div class="col-9 align-items-center d-flex">
+          <el-select
+            v-model="form.mapel_id"
+            placeholder="Pilih Mapel"
+            style="width: 100%"
+            filterable
+          >
+            <el-option
+              v-for="mapel in props.dataOption.mapelOption"
+              :key="mapel.mapel_id"
+              :label="mapel.mapel_nama"
+              :value="mapel.mapel_id"
+            />
+          </el-select>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 align-items-center d-flex">
+          <p class="m-0 fs-4 fw-bold">Judul Materi</p>
+        </div>
+        <div class="col-9 align-items-center d-flex gap-4">
+          <el-input v-model="form.materi_judul" placeholder="Judul Materi" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 align-items-center d-flex">
+          <p class="m-0 fs-4 fw-bold">URL Youtube</p>
+        </div>
+        <div class="col-9 align-items-center d-flex gap-4">
+          <el-input v-model="form.materi_file" placeholder="Link Materi Youtube" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3 align-items-center d-flex">
+          <p class="m-0 fs-4 fw-bold">Status Materi</p>
+        </div>
+        <div class="col-9 align-items-center d-flex gap-4">
+          <el-select class="w-100" v-model="form.materi_status" placeholder="Pilih Status">
+            <el-option label="Aktif" value="1" />
+            <el-option label="Non Aktif" value="0" />
+          </el-select>
+        </div>
+      </div>
+    </div>
+	</Modal>
+</template>
--- src/views/lms/materi-belajar/video/Index.vue
+++ src/views/lms/materi-belajar/video/Index.vue
@@ -9,10 +9,11 @@
   import { useToast } from "vue-toast-notification"
   import { deleteConfirmation } from "@/core/helpers/deleteconfirmation";
 import moment from "moment";
+import FormModal from "./FormModal.vue";
   
   onMounted(() => {
     setCurrentPageBreadcrumbs("Materi Video", ['LMS', "Materi Belajar"]);
-    // getData()
+    getData()
   })
 
   function getVideoData (payload) {
@@ -46,6 +47,8 @@ import moment from "moment";
     })
   }
 
+  const tableRef = ref()
+
   const guruFilter = ref()
   const kelasFilter = ref()
   const mapelFilter = ref()
@@ -53,6 +56,9 @@ import moment from "moment";
   const guruOption = ref([])
   const kelasOption = ref([])
   const mapelOption = ref([])
+
+  const formMode = ref()
+  const activeData = ref()
 
   const videoData = reactive({
     columns: [
@@ -81,94 +87,123 @@ import moment from "moment";
       })
     })
   }
+
+  function handleEdit (row) {
+    activeData.value = row
+    formMode.value = 'Edit Data'
+  }
+  function handleFormClose (row) {
+    activeData.value = {}
+    formMode.value = ''
+  }
 </script>
 
 <template>
-  <div class="card mb-5 mb-xxl-8">
-    <div class="card-body pt-5 pb-5">
-      <div class="page-content">
-        <div class="d-flex flex-wrap justify-content-between align-items-center">
-          <div class="d-flex gap-4">
-            <h2 class="fs-1 fw-bold py-6">Video Materi Belajar</h2>
-          </div>
-
-          <div class="position-relative d-flex ">
-            <router-link to="/lms/materi-belajar/video/tambah" class="btn btn-primary d-flex gap-3 align-items-center w-auto">
-              <i class="bi bi-plus fs-1"></i>
-              <span>
-                Tambah Video
-              </span>
-            </router-link>
-          </div>
-        </div>
-        <div class="separator border-black-50 border-2 my-6"></div>
-        <!-- <div class="d-flex flex-wrap justify-content-between align-items-center mb-4">
-          <div class="d-flex gap-4">
-            <div>
-              <FilterSelect searchable v-model:filterValue="guruFilter" placeholder="Pilih Guru" @changeFilter="getVideoData()">
-                <el-option
-                  v-for="guru in guruOption"
-                  :key="guru.user_id"
-                  :label="guru.user_nama"
-                  :value="guru.user_id"
-                />
-              </FilterSelect>
+  <div>
+    <div class="card mb-5 mb-xxl-8">
+      <div class="card-body pt-5 pb-5">
+        <div class="page-content">
+          <div class="d-flex flex-wrap justify-content-between align-items-center">
+            <div class="d-flex gap-4">
+              <h2 class="fs-1 fw-bold py-6">Video Materi Belajar</h2>
             </div>
-            <div>
-              <FilterSelect v-model:filterValue="kelasFilter" placeholder="Pilih Kelas" @changeFilter="getVideoData()">
-                <el-option
-                  v-for="kelas, index in kelasOption"
-                  :key="kelas.kelas_id"
-                  :label="kelas.kelas_nama"
-                  :value="kelas.kelas_id"
-                />
-              </FilterSelect>
-            </div>
-            <div>
-              <FilterSelect v-model:filterValue="mapelFilter" placeholder="Pilih Mapel" @changeFilter="getVideoData()">
-                <el-option
-                  v-for="mapel, index in mapelOption"
-                  :key="mapel.mapel_id"
-                  :label="mapel.mapel_nama"
-                  :value="mapel.mapel_id"
-                />
-              </FilterSelect>
+  
+            <div class="position-relative d-flex ">
+              <button @click="formMode = 'Tambah Data'" class="btn btn-primary d-flex gap-3 align-items-center w-auto">
+                <i class="bi bi-plus fs-1"></i>
+                <span>
+                  Tambah Video
+                </span>
+              </button>
+              <!-- <router-link to="/lms/materi-belajar/video/tambah" class="btn btn-primary d-flex gap-3 align-items-center w-auto">
+                <i class="bi bi-plus fs-1"></i>
+                <span>
+                  Tambah Video
+                </span>
+              </router-link> -->
             </div>
           </div>
-        </div> -->
-        <div class="mb-5 mb-xxl-8">
-          <ServerSideTable
-            :totalRows="videoData.totalRows || 0"
-            :columns="videoData.columns"
-            :rows="videoData.rows"
-            @loadItems="getVideoData"
-          >
-            <template #table-row="{column, row}">
-              <div v-if="column.field == 'materi_status'">
-                <span :class="'badge badge-light-' + (row.materi_status == 1 ? 'success' : 'danger')">{{row.materi_status == 1 ? 'Aktif' : 'Non Aktif'}}</span>
+          <div class="separator border-black-50 border-2 my-6"></div>
+          <!-- <div class="d-flex flex-wrap justify-content-between align-items-center mb-4">
+            <div class="d-flex gap-4">
+              <div>
+                <FilterSelect searchable v-model:filterValue="guruFilter" placeholder="Pilih Guru" @changeFilter="getVideoData()">
+                  <el-option
+                    v-for="guru in guruOption"
+                    :key="guru.user_id"
+                    :label="guru.user_nama"
+                    :value="guru.user_id"
+                  />
+                </FilterSelect>
               </div>
-              <div v-if="column.field == 'materi_file'">
-                {{'https://www.youtube.com/watch?v='+row.materi_file}}
+              <div>
+                <FilterSelect v-model:filterValue="kelasFilter" placeholder="Pilih Kelas" @changeFilter="getVideoData()">
+                  <el-option
+                    v-for="kelas, index in kelasOption"
+                    :key="kelas.kelas_id"
+                    :label="kelas.kelas_nama"
+                    :value="kelas.kelas_id"
+                  />
+                </FilterSelect>
               </div>
-              <div v-if="column.field == 'materi_create_date'">
-                {{dateFormating(row.materi_create_date)}}
+              <div>
+                <FilterSelect v-model:filterValue="mapelFilter" placeholder="Pilih Mapel" @changeFilter="getVideoData()">
+                  <el-option
+                    v-for="mapel, index in mapelOption"
+                    :key="mapel.mapel_id"
+                    :label="mapel.mapel_nama"
+                    :value="mapel.mapel_id"
+                  />
+                </FilterSelect>
               </div>
-              <div v-if="column.field == 'action'">
-                <router-link :to="`/lms/materi-belajar/video/edit/${row.materi_id}`" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2">
-                  <span class="svg-icon svg-icon-3">
-                    <inline-svg src="media/icons/duotune/art/art005.svg" />
-                  </span>
-                </router-link>
-                <button @click="deleteData(row.materi_id)" class="btn btn-icon btn-bg-light btn-active-color-danger btn-sm">
-                  <span class="svg-icon svg-icon-3">
-                    <inline-svg src="media/icons/duotune/general/gen027.svg"/>
-                  </span>
-                </button>
-              </div>
-            </template>
-          </ServerSideTable>
+            </div>
+          </div> -->
+          <div class="mb-5 mb-xxl-8">
+            <ServerSideTable
+              ref="tableRef"
+              :totalRows="videoData.totalRows || 0"
+              :columns="videoData.columns"
+              :rows="videoData.rows"
+              @loadItems="getVideoData"
+            >
+              <template #table-row="{column, row}">
+                <div v-if="column.field == 'materi_status'">
+                  <span :class="'badge badge-light-' + (row.materi_status == 1 ? 'success' : 'danger')">{{row.materi_status == 1 ? 'Aktif' : 'Non Aktif'}}</span>
+                </div>
+                <div v-if="column.field == 'materi_file'">
+                  {{'https://www.youtube.com/watch?v='+row.materi_file}}
+                </div>
+                <div v-if="column.field == 'materi_create_date'">
+                  {{dateFormating(row.materi_create_date)}}
+                </div>
+                <div v-if="column.field == 'action'">
+                  <button @click="handleEdit(row)" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2">
+                    <span class="svg-icon svg-icon-3">
+                      <inline-svg src="media/icons/duotune/art/art005.svg" />
+                    </span>
+                  </button>
+                  <!-- <router-link :to="`/lms/materi-belajar/video/edit/${row.materi_id}`" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-2">
+                    <span class="svg-icon svg-icon-3">
+                      <inline-svg src="media/icons/duotune/art/art005.svg" />
+                    </span>
+                  </router-link> -->
+                  <button @click="deleteData(row.materi_id)" class="btn btn-icon btn-bg-light btn-active-color-danger btn-sm">
+                    <span class="svg-icon svg-icon-3">
+                      <inline-svg src="media/icons/duotune/general/gen027.svg"/>
+                    </span>
+                  </button>
+                </div>
+              </template>
+            </ServerSideTable>
+          </div>
         </div>
       </div>
     </div>
+    <FormModal 
+			:mode="formMode"
+			:activeData="activeData"
+      :dataOption="{kelasOption: kelasOption, mapelOption: mapelOption}"
+			@close="handleFormClose"
+			@submit="tableRef.loadItems()" />
   </div>
 </template>
