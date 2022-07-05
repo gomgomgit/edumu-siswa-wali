5f8c82b7e414293b54cce278a1cf1fc577052a0a
gomgomgit <thechocohead@gmail.com>
2022-07-05 15:44:51 +0700

    login

--- src/layout/header/partials/UserMenu.vue
+++ src/layout/header/partials/UserMenu.vue
@@ -17,10 +17,6 @@
         <div class="d-flex flex-column">
           <div class="fw-bolder d-flex align-items-center fs-5">
             {{userData.user_nama}}
-            <span
-              class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2"
-              >Pro</span
-            >
           </div>
           <a href="#" class="fw-bold text-muted text-hover-primary fs-7"
             >{{userData.user_level}}</a
