ac4e7c03ce0c3f5c6951f452e032e823355a72c6
gomgomgit <thechocohead@gmail.com>
2022-08-21 07:20:44 +0700

    sign in process

--- src/views/crafted/authentication/basic-flow/SignInProcess.vue
+++ src/views/crafted/authentication/basic-flow/SignInProcess.vue
@@ -16,29 +16,23 @@ import {
   loaderLogo,
 } from "@/core/helpers/config";
 import { async } from "validate.js";
+import { inject } from 'vue'
 
 const store = useStore();
 const router = useRouter();
 const route = useRoute();
 
-const loginData = route.params.data;
+const cryoptojs = inject('cryptojs')
+
+const loginData = route.query.data;
 store.dispatch(Actions.ADD_BODY_CLASSNAME, "page-loading");
 
 function loginProcess() {
-  var data = QueryString.parse(loginData)
-  store.dispatch(Actions.LOGIN, data)
 
-  // Swal.fire({
-  //   text: "You have successfully logged in!",
-  //   icon: "success",
-  //   buttonsStyling: false,
-  //   confirmButtonText: "Ok, got it!",
-  //   customClass: {
-  //     confirmButton: "btn fw-bold btn-light-primary",
-  //   },
-  //   }).then(function () {
-  //     router.push({ name: "dashboard" });
-  // });
+  var bytes = cryoptojs.AES.decrypt(loginData.replace(/ /g, '+'), "edumuv2").toString(cryoptojs.enc.Utf8)
+  var data = QueryString.parse(bytes);
+
+  store.dispatch(Actions.LOGIN, data)
 }
 onMounted(async() => {
   await loginProcess()
