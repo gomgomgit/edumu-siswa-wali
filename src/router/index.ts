c19eef5751d299fbeb263521f02685e771633367
gomgomgit <thechocohead@gmail.com>
2022-08-31 15:50:24 +0700

    hide sign in

--- src/router/index.ts
+++ src/router/index.ts
@@ -231,30 +231,30 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import("@/views/crafted/authentication/basic-flow/SignInProcess.vue"),
   },
-  {
-    path: "/",
-    component: () => import("@/components/page-layouts/Auth.vue"),
-    children: [
-      {
-        path: "/sign-in",
-        name: "sign-in",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/SignIn.vue"),
-      },
-      {
-        path: "/sign-up",
-        name: "sign-up",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/SignUp.vue"),
-      },
-      {
-        path: "/password-reset",
-        name: "password-reset",
-        component: () =>
-          import("@/views/crafted/authentication/basic-flow/PasswordReset.vue"),
-      },
-    ],
-  },
+  // {
+  //   path: "/",
+  //   component: () => import("@/components/page-layouts/Auth.vue"),
+  //   children: [
+  //     {
+  //       path: "/sign-in",
+  //       name: "sign-in",
+  //       component: () =>
+  //         import("@/views/crafted/authentication/basic-flow/SignIn.vue"),
+  //     },
+  //     {
+  //       path: "/sign-up",
+  //       name: "sign-up",
+  //       component: () =>
+  //         import("@/views/crafted/authentication/basic-flow/SignUp.vue"),
+  //     },
+  //     {
+  //       path: "/password-reset",
+  //       name: "password-reset",
+  //       component: () =>
+  //         import("@/views/crafted/authentication/basic-flow/PasswordReset.vue"),
+  //     },
+  //   ],
+  // },
   {
     // the 404 route, when none of the above matches
     path: "/404",
